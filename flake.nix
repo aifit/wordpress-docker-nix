9e28bd973aa0e3db26653ed0c408b4db79c89e71
Yahya J. Aifit <yahya@yja.me>
2025-10-13 14:02:09 +0700

    checkpoint

--- flake.nix
+++ flake.nix
@@ -38,12 +38,21 @@
             echo ""
             if [[ "$(uname)" == "Darwin" ]]; then
               echo -e " Detected system: \e[32mmacOS\e[0m"
+              echo " Start Colima with resources, e.g.:"
+              echo -e " $ \e[36mcolima start --cpu 2 --memory 4 --disk 15\e[0m"
+              echo " Then run:"
+              echo -e " $ \e[36mdocker-compose up -d\e[0m"
+              echo ""
             else
               echo " Detected system: \e[32mLinux\e[0m"
+              echo " Make sure Docker daemon is running:"
+              echo -e " $\e[36mdocker --version\e[0m"
+              echo " Then run:"
+              echo -e " $ \e[36mdocker-compose up -d\e[0m"
             fi
             echo ""
-            echo -e " 🎬 See the demo video:"
-            echo -e "   \e[34mhttps://github.com/aifit/wordpress-docker-nix/blob/main/README.md#-demo-video-youtube\e[0m"
+            echo -e "Readme:"
+            echo -e "   \e[34mhttps://github.com/aifit/wordpress-docker-nix/blob/main/README.md\e[0m"
             echo ""
           '';
         };
